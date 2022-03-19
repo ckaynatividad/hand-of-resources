ba5613124cbf3b14a6c616f0402c61d9dfbded57
ckaynatividad <natividad.ckay@gmail.com>
2022-03-18 17:12:12 -0700

    first test passing for cat create

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,15 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('creates cat', async () => {
+    const expected = {
+      name: 'Cardamon',
+      age: 4,
+      type: 'Himalayan',
+    };
+    const res = await request(app).post('/api/v1/cats').send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS cats;
+
+CREATE TABLE cats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INT NOT NULL CHECK (age > -1),
+    type TEXT
+);
+
+INSERT INTO
+    cats (name, age, type)
+VALUES
+    ('Cardamon', 4, 'Himalayan'),
+    ('Bucket', 5, 'Scottish Fold Tabby');
