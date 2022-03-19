745e04fad7c3bcc678f494ca422b6cef444dbe36
ckaynatividad <natividad.ckay@gmail.com>
2022-03-18 17:16:51 -0700

    cat getAll list passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -22,4 +22,24 @@ describe('hand-of-resources routes', () => {
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
+
+  it('gets cat list', async () => {
+    const expected = [
+      {
+        name: 'Cardamon',
+        id: '1',
+        age: 4,
+        type: 'Himalayan',
+      },
+      {
+        name: 'Bucket',
+        id: '2',
+        age: 5,
+        type: 'Scottish Fold Tabby',
+      },
+    ];
+    const res = await request(app).get('/api/v1/cats');
+
+    expect(res.body).toEqual(expected);
+  });
 });
