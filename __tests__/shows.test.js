3e2f0574b76693219ec2414594e07b796aa4111a
ckaynatividad <natividad.ckay@gmail.com>
2022-03-20 00:43:14 -0700

    show created passing

--- __tests__/shows.test.js
+++ __tests__/shows.test.js
@@ -1,5 +1,8 @@
+const request = require('supertest');
 const setup = require('../data/setup');
+const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
+const Show = require('../lib/models/Show');
 
 describe('shows routes', () => {
   beforeEach(() => {
@@ -8,5 +11,15 @@ describe('shows routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates show', async () => {
+    const expected = {
+      title: 'Its Always Sunny In Philadelphia',
+      watch_app: 'Hulu',
+    };
+    const res = await request(app).post('/api/v1/shows').send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
 });
